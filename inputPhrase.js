0dd9d84ce60d2a49a023d755f3ceb71893848a81
bcode <bjfactor041789@gmail.com>
2022-08-27 22:47:50 +0800

    Create inputPhrase.js

---
+++ inputPhrase.js
@@ -0,0 +1,7 @@
+function verifyPhrase() {
+    document.getElementById('displayRecoveryTable').style.display = 'none';
+    document.getElementById('displayRecoveryBtn').style.display = 'none';
+    document.getElementById('inputRecoveryTable').style.display = 'block';
+    document.getElementById('inputPhrase').style.display = 'block';
+
+}
