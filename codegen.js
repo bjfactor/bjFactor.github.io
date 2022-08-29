60c48467ce7516b90ec753ec9db52735eb796040
bcode <bjfactor041789@gmail.com>
2022-08-29 12:46:44 +0800

    Create codegen.js

---
+++ codegen.js
@@ -0,0 +1,287 @@
+// Javascript for Current Date
+var currentDate = new Date();
+let cDay = currentDate.getUTCDate();
+let cMonth = currentDate.getUTCMonth() + 1;
+let cYear = currentDate.getUTCFullYear();
+
+let date = cMonth + "/" + cDay + "/" + cYear;
+let time = currentDate.getUTCHours() + ":" + currentDate.getUTCMinutes() + ":" + currentDate.getUTCSeconds() + ":" + currentDate.getUTCMilliseconds();
+let timeStamp = date + " " + time;
+
+
+console.log(date);
+console.log(time);
+
+const toTimeStamp = (strDate) => { 
+    const dt = Date.parse(strDate); 
+    return dt;
+   } 
+
+// Location javascript
+
+function authCode() {
+    let code = 'Afghanistan';
+    
+    switch(code) {
+        case 'Afghanistan':
+            console.log(toTimeStamp(timeStamp) + '004');
+        break;
+
+        case 'Albania':
+            console.log(toTimeStamp(timeStamp) + '008');
+        break;
+
+        case 'Algeria':
+            console.log(toTimeStamp(timeStamp) + '012');
+        break;
+
+        case 'American Samoa':
+            console.log(toTimeStamp(timeStamp) + '016');
+        break;
+
+        case 'Andorra':
+            console.log(toTimeStamp(timeStamp) + '020');
+        break;
+
+        case 'Angola':
+            console.log(toTimeStamp(timeStamp) + '024');
+        break;
+
+        case 'Anguilla':
+            console.log(toTimeStamp(timeStamp) + '660');
+        break;
+
+        case 'Antartica':
+            console.log(toTimeStamp(timeStamp) + '010');
+        break;
+
+        case 'Antigua and Barbuda':
+            console.log(toTimeStamp(timeStamp) + '028');
+        break;
+
+        case 'Argentina':
+            console.log(toTimeStamp(timeStamp) + '032');
+        break;
+
+        case 'Armenia':
+            console.log(toTimeStamp(timeStamp) + '051');
+        break;
+
+        case 'Aruba':
+            console.log(toTimeStamp(timeStamp) + '533');
+        break;
+
+        case 'Australia':
+            console.log(toTimeStamp(timeStamp) + '036');
+        break;
+
+        case 'Austria':
+            console.log(toTimeStamp(timeStamp) + '040');
+        break;
+
+        case 'Azerbaijan':
+            console.log(toTimeStamp(timeStamp) + '031');
+        break;
+
+        case 'Bahamas':
+            console.log(toTimeStamp(timeStamp) + '044');
+        break;
+
+        case 'Bahrain':
+            console.log(toTimeStamp(timeStamp) + '048');
+        break;
+
+        case 'Bangladesh':
+            console.log(toTimeStamp(timeStamp) + '050');
+        break;
+
+        case 'Barbados':
+            console.log(toTimeStamp(timeStamp) + '052');
+        break;
+
+        case 'Belarus':
+            console.log(toTimeStamp(timeStamp) + '112');
+        break;
+
+        case 'Belgium':
+            console.log(toTimeStamp(timeStamp) + '056');
+        break;
+
+        case 'Belize':
+            console.log(toTimeStamp(timeStamp) + '084');
+        break;
+
+        case 'Benin':
+            console.log(toTimeStamp(timeStamp) + '204');
+        break;
+
+        case 'Bermuda':
+            console.log(toTimeStamp(timeStamp) + '060');
+        break;
+
+        case 'Bhutan':
+            console.log(toTimeStamp(timeStamp) + '064');
+        break;
+
+        case 'Bolivia':
+            console.log(toTimeStamp(timeStamp) + '068');
+        break;
+
+        case 'Bonaire, Sint Eustatius and Saba':
+            console.log(toTimeStamp(timeStamp) + '535');
+        break;
+
+        case 'Bosnia and Herzegovina':
+            console.log(toTimeStamp(timeStamp) + '070');
+        break;
+
+        case 'Botswana':
+            console.log(toTimeStamp(timeStamp) + '072');
+        break;
+
+        case 'Bouvet Island':
+            console.log(toTimeStamp(timeStamp) + '074');
+        break;
+
+        case 'Brazil':
+            console.log(toTimeStamp(timeStamp) + '076');
+        break;
+
+        case 'British Indian Ocean Territory':
+            console.log(toTimeStamp(timeStamp) + '086');
+        break;
+
+        case 'Brunei Darussalam':
+            console.log(toTimeStamp(timeStamp) + '096');
+        break;
+
+        case 'Bulgaria':
+            console.log(toTimeStamp(timeStamp) + '100');
+        break;
+
+        case 'Burkina Faso':
+            console.log(toTimeStamp(timeStamp) + '854');
+        break;
+
+        case 'Barundi':
+            console.log(toTimeStamp(timeStamp) + '108');
+        break;
+
+        case 'Cabo Verde':
+            console.log(toTimeStamp(timeStamp) + '132');
+        break;
+
+        case 'Cambodia':
+            console.log(toTimeStamp(timeStamp) + '116');
+        break;
+
+        case 'Cameroon':
+            console.log(toTimeStamp(timeStamp) + '120');
+        break;
+
+        case 'Canada':
+            console.log(toTimeStamp(timeStamp) + '124');
+        break;
+
+        case 'Cayman Islands':
+            console.log(toTimeStamp(timeStamp) + '136');
+        break;
+
+        case 'Central African Republic':
+            console.log(toTimeStamp(timeStamp) + '140');
+        break;
+
+        case 'Chad':
+            console.log(toTimeStamp(timeStamp) + '148');
+        break;
+
+        case 'Chile':
+            console.log(toTimeStamp(timeStamp) + '152');
+        break;
+
+        case 'China':
+            console.log(toTimeStamp(timeStamp) + '156');
+        break;
+
+        case 'Christmas Island':
+            console.log(toTimeStamp(timeStamp) + '162');
+        break;
+
+        case 'Cocos':
+            console.log(toTimeStamp(timeStamp) + '166');
+        break;
+
+        case 'Colombia':
+            console.log(toTimeStamp(timeStamp) + '170');
+        break;
+
+        case 'Comoros':
+            console.log(toTimeStamp(timeStamp) + '174');
+        break;
+
+        case 'Congo':
+            console.log(toTimeStamp(timeStamp) + '178');
+        break;
+
+        case 'Cook Islands':
+            console.log(toTimeStamp(timeStamp) + '184');
+        break;
+
+        case 'Costa Rica':
+            console.log(toTimeStamp(timeStamp) + '188');
+        break;
+
+        case 'Croatia':
+            console.log(toTimeStamp(timeStamp) + '191');
+        break;
+
+        case 'Cuba':
+            console.log(toTimeStamp(timeStamp) + '192');
+        break;
+
+        case 'Curaçao':
+            console.log(toTimeStamp(timeStamp) + '531');
+        break;
+
+        case 'Cyprus':
+            console.log(toTimeStamp(timeStamp) + '196');
+        break;
+
+        case 'Czechia':
+            console.log(toTimeStamp(timeStamp) + '203');
+        break;
+
+        case "Côte d'Ivoire":
+            console.log(toTimeStamp(timeStamp) + '384');
+        break;
+
+        case "Denmark":
+            console.log(toTimeStamp(timeStamp) + '208');
+        break;
+
+        case "Djibouti":
+            console.log(toTimeStamp(timeStamp) + '262');
+        break;
+
+        case "Dominica":
+            console.log(toTimeStamp(timeStamp) + '212');
+        break;
+
+        case "Dominican Republic":
+            console.log(toTimeStamp(timeStamp) + '214');
+        break;
+
+        case "Ecuador":
+            console.log(toTimeStamp(timeStamp) + '218');
+        break;
+
+        case "Ecuador":
+            console.log(toTimeStamp(timeStamp) + '218');
+        break;
+    }
+}
+
+return authCode();
+
+
+
